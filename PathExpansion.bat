4ea9a74e5efef336aeda95e164cc8d8c6f9e54fd
tdinh <trieudp@gmail.com>
2020-08-05 17:26:52 +0200

    description changed

--- PathExpansion.bat
+++ PathExpansion.bat
@@ -2,8 +2,8 @@
 echo %%0                       %0
 echo Drive:                   %~d0%
 echo Path without drive:      %~p0%
-echo Drive and path:          %~dp0%
-echo Path with drive:         %~f0%
+echo Drive and path location: %~dp0%
+echo Path full:               %~f0%
 echo Filename without suffix: %~n0%
 echo Suffix (incl. dot):      %~x0%
 echo Short name:              %~s0%

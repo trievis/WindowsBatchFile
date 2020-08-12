cf5159b15dac21ca1a1cf76a5245a53db5c55d68
tdinh <trieudp@gmail.com>
2020-08-12 13:29:17 +0200

    update expression of syntax in the description.

--- PathExpansion.bat
+++ PathExpansion.bat
@@ -1,12 +1,12 @@
 @echo off
-echo %%0                       %0
-echo Drive:                   %~d0%
-echo Path without drive:      %~p0%
-echo Drive and path location: %~dp0%
-echo Path full:               %~f0%
-echo Filename without suffix: %~n0%
-echo Suffix (incl. dot):      %~x0%
-echo Short name:              %~s0%
-echo Attributes:              %~a0%
-echo Last modification:       %~t0%
-echo File size:               %~z0%
+echo %%0                              %0
+echo Drive %%~d0:                     %~d0%
+echo Path without drive %%~p0:        %~p0%
+echo Drive and path location %%~dp0%: %~dp0%
+echo Path full %%~f0%:                %~f0%
+echo Filename without suffix %%~n0%:  %~n0%
+echo Suffix (incl. dot) %%~x0%:       %~x0%
+echo Short name %%~s0%:               %~s0%
+echo Attributes %%~a0%:               %~a0%
+echo Last modification %%~t0%:        %~t0%
+echo File size %%~z0%:                %~z0%
